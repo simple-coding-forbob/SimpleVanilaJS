f110db47a0ee1a74c1f41d8913d79a87a1ac5e29
simple-coding-forbob <96178104+simple-coding-forbob@users.noreply.github.com>
2025-10-10 17:44:48 +0900

    1
    1

---
+++ 03_JS/js/08_quiz.js
@@ -0,0 +1,9 @@
+// TODO: 숨기기
+function hideMenu2() {
+  document.querySelector("#menu2").style.display = "none";
+}
+
+// TODO: 보이기
+function showMenu2() {
+  document.querySelector("#menu2").style.display = "block";
+}
