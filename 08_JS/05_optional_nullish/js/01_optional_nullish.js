ee8cf7cc957d92cae34cabe6d9e00dc9414f4711
forbob <forbob@naver.com>
2025-10-16 08:13:02 +0900

    1
    1

--- 08_JS/05_optional_nullish/js/01_optional_nullish.js
+++ 08_JS/05_optional_nullish/js/01_optional_nullish.js
@@ -5,15 +5,12 @@
 // => 서비스가 갑자기 다운(예: 카카오톡이 서비스 다운)
 // 코딩 : null 에러를 회피해서 코딩을 전통적으로 했음
 // 아래 예)
-let num = null;
-// 미리 변수의 값에 null 이 있는지 체크했음
-if( num == null) {
-    console.log("이 기능은 실행하지 않습니다.");
-}
+// let num = null;
+// // 미리 변수의 값에 null 이 있는지 체크했음
+// if( num == null) {
+//     console.log("num 변수에 null 이 있습니다.");
+// }
 
-// 개선된 예제 : 옵셔널 널리쉬 사용
-// user 가 null 이면 강제로 undefined 값으로 변경함
-// 사용법 : 변수명?.속성명
 let user = null; // 객체 , address 속성이 있는지 없는지 체크
 // 객체의 속성 호출 : user.address => user = null;
 //                   null.address (에러 : 서비스 다운)
