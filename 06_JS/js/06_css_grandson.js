b16ff2c70d1d1975fd522caf366bab3fe3480c14
simple-coding-forbob <96178104+simple-coding-forbob@users.noreply.github.com>
2026-03-03 08:52:51 +0900

    1
    1

--- 06_JS/js/06_css_grandson.js
+++ 06_JS/js/06_css_grandson.js
@@ -1,1 +1,3 @@
-document.querySelector("#header h1").style.backgroundColor = "yellow";
+let a=document.querySelectorAll("#header h1"); // 자식 h1, 손자 h1 2개(배열임)
+a[0].style.backgroundColor = "yellow";
+a[1].style.backgroundColor = "yellow";
--- 06_JS/js/11_hover_show_hide.js
+++ 06_JS/js/11_hover_show_hide.js
@@ -1,11 +1,8 @@
 // 11_hover_show_hide.js
-const subMenu = document.querySelector("#sub-menu");
-const dropDown = document.querySelector("#drop-down");
-
 function showMenu() {
-  dropDown.style.display = "block"; // 마우스 올리면 보이기
+  document.querySelector("#drop-down").style.display = "block"; // 마우스 올리면 보이기
 }
 
 function hideMenu() {
-  dropDown.style.display = "none"; // 마우스 나가면 숨기기
+  document.querySelector("#drop-down").style.display = "none"; // 마우스 나가면 숨기기
 }
